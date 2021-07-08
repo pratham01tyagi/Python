528e8d11d34da13c3b52ecc6ee59ac9896d6eee2
Pratham Tyagi <77679553+pratham01tyagi@users.noreply.github.com>
2021-07-08 13:12:35 +0530

    2nd python notes and code file

---
+++ Learning Python/python2.py
@@ -0,0 +1,41 @@
+'''######################################## code by pratham tyagi################################################################'''
+'''hi this is my 2st coading worksheet in this i will teach python '''
+'''################################################################################################################################'''
+
+
+
+
+'''################################################################################################################################'''
+#Variables
+
+'''
+there are few rules for using variables-->
+1.Variable name must begain with a  letter or an underscore_character.
+2.they can have no also but not at the begnning.
+3.they are case sensitive eg:- great & Great are 2 different variables.
+4.SYNTAX-->| variable1="variable" |
+           | print(variable1)     |
+'''
+
+'''
+#if in python we want to know the type of the variable being used --
+name = "tim"
+age =30
+print (type(name))
+print(type(age))
+# in python we could change the type or overwrite the type of variable also--
+age_variable ="30 years"
+print(type(age_variable)) 
+# now age which was int earlyer became str.
+'''
+
+
+'''################################################################################################################################'''
+#Data type
+'''
+here  we have 3 numeric data types:
+1.int 
+2.float   eg-2.2222222222 upto 308 decimal  etc.
+3.complex eg-
+'''
+'''################################################################################################################################'''
