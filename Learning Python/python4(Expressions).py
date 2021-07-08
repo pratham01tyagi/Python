234e5b14a62aedaac7eb9eaf92c084e479c5c92f
Pratham Tyagi <77679553+pratham01tyagi@users.noreply.github.com>
2021-07-09 01:35:22 +0530

    this file has about type of expressions in python.

---
+++ Learning Python/python4(Expressions).py
@@ -0,0 +1,35 @@
+#############################################################################################################################################
+#                                                       EXPRESSIONS
+
+'''
+#in python anything that could be calculated is an expression some examples are-
+
+a=100 #this is assingment not expression
+b=20  #this is assingment not expression
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a//b)
+print(a%b)
+
+# above from line (a+b)to (a%b) are expressions...
+#NOTE - i=10
+#       print(i) both the lines are not expression. 
+
+'''
+
+#                                                       OPERATOR PRECEDENCE
+
+'''
+
+#it tell about the priority of the operators they follow -"BOEDMAS" or "BEDMAS" both are same.
+#NOTE- here addition & subraction / division & multiplication both have same priority.
+#NOTE- in expression with the operations with same priorities they are evaluated from left to right. In python. 
+a=100
+b=10
+print(a/(b*5)/b)  #output-0.2
+
+'''
+#############################################################################################################################################
+
