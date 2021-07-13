e9cc6a77dfaf55944889b9222e96da94f506359b
Pratham Tyagi <77679553+pratham01tyagi@users.noreply.github.com>
2021-07-13 08:52:59 +0530

    about block statments.

---
+++ Learning Python/python5/python5.0(Blocks & Statments).py
@@ -0,0 +1,51 @@
+###########################################################################################################################################
+#                                        BLOCK & STATMENT
+'''
+#Indenting-meaning as in c or java we use to put  statment next to  loops in {} here we don't do that 
+#rather that process is called indentation.
+#Here we are doing Indenting also note that after for loop we put : for telling python of INDENTATION.
+for i in range(0,10):
+    print(f"hello {i:2} time")  #also to do indentation manually press Tab key in starting of that line.
+print("#"*80)
+
+#output-
+#hello  0 time
+#hello  1 time
+#hello  2 time
+#hello  3 time
+#hello  4 time
+#hello  5 time
+#hello  6 time
+#hello  7 time
+#hello  8 time
+#hello  9 time
+################################################################################
+
+#NOTE-> if both the print statment have been intended then the result would be as follow
+
+for i in range(0,10):
+    print(f"hello {i:2} time")  #also to do indentation manually press Tab key in starting of that line.
+    print("#"*80)
+
+#output-
+#hello  0 time
+################################################################################
+#hello  1 time
+################################################################################
+#hello  2 time
+################################################################################
+#hello  3 time
+################################################################################
+#hello  4 time
+################################################################################
+#hello  5 time
+################################################################################
+#hello  6 time
+################################################################################
+#hello  7 time
+################################################################################
+#hello  8 time
+################################################################################
+#hello  9 time
+################################################################################
+'''
