1b1a16db918f9ad53cd7d29c639a256266acb092
Pratham Tyagi <77679553+pratham01tyagi@users.noreply.github.com>
2021-07-15 20:39:34 +0530

    using and & or.

---
+++ Learning Python/python5/python5.2(and-or-in).py
@@ -0,0 +1,21 @@
+############################################################################################################################
+#                                 USING IN AND OR CONDITIONS.
+'''
+age= int(input("tell your age?"))
+
+if age>=16 and age<=65:
+    print("hello")
+
+# simply chained comparison-
+age= int(input("tell your age?"))
+
+if 16<=age<=65:
+    print("hello")
+
+#using or-
+age= int(input("tell your age?"))
+
+if age<16 or age>65:
+    print("bye")
+'''
+##################################################################################################################
