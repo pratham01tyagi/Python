0a19ddfc525195cb4ccd1c2d4aa62ce762f66cf4
Pratham Tyagi <77679553+pratham01tyagi@users.noreply.github.com>
2021-07-23 18:06:15 +0530

    the number game build by importing random

---
+++ Learning Python/python5/python5.8(importing_random).py
@@ -0,0 +1,26 @@
+###############################################################################################################
+#                                    RANDOM & IMPORT
+
+import random
+highest = int(input("enter the highest range :"))
+ans = random.randint(0, highest)
+#here we are calling "randint" function from "random" module by dot notation.
+print(ans)
+guess = 0
+print(f"enter no betwen 0 to {highest} ")
+while guess != ans:
+        guess = int(input(f"enter no"))
+        if guess == ans:
+            print(f"you guessed it right the answer is {ans}")
+        else:
+            if ans<guess:
+                print("guess low")
+                
+            else:
+                print("guess high")
+                
+
+                
+            
+            
+            
