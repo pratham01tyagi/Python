5b8a811de9592abb6b6a8a2bf24dba3e3750d81f
Pratham Tyagi <77679553+pratham01tyagi@users.noreply.github.com>
2021-07-25 15:48:10 +0530

    binary search and practice of it in python.

---
+++ Learning Python/python5/python5.9(binary search).py
@@ -0,0 +1,6 @@
+#############################################################################################################
+#                                        BINARY SEARCH
+
+#it could help with ordered data only. here we split the data into half each time to find the required data.
+# mid point is given by the formula -{lowerpoint + (higherpoint - lowerpoint)//2}
+# example of this is hi lo game in 5.10
